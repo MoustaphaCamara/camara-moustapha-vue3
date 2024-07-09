219f61cac11b79ebd1a9f45a1084c0c6f9c9a510
MoustaphaCamara <cmr.mous@gmail.com>
2024-07-09 15:39:22 +0200

    feat(api): fetch amiibos

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,11 +1,47 @@
 <script setup lang="ts">
 import Footer from "./Footer.vue";
-import Header from "./Header.vue";
 import Character from "./Character.vue";
+import ky from "ky";
+import { onMounted, ref } from "vue";
+
+interface Amiibo {
+  amiiboSeries: string;
+  character: string;
+  gameSeries: string;
+  head: string;
+  image: string;
+  name: string;
+  release: {
+    au?: string;
+    eu?: string;
+    jp?: string;
+    na?: string;
+  };
+  tail: string;
+  type: string;
+}
+
+const amiibos = ref<Amiibo[]>([]);
+
+const fetchData = async (): Promise<Amiibo> => {
+  try {
+    const fetch = await ky
+      .get("https://www.amiiboapi.com/api/amiibo/?character=Mewtwo")
+      .json();
+    const { amiibo } = fetch;
+    amiibos.value = amiibo;
+    console.log(amiibos.value);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
 </script>
 
 <template>
-  <!-- ici vous pouvez choisir 3 amiibos au hasard ou juste vos trois préférés -->
   <section id="intro" class="container">
     <div class="row">
       <Character />
